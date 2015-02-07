a46220e9c409ffa12d968d6a6b2301d8c341d27b
Manan Shah <manan.shah.777@gmail.com>
2015-02-06 17:30:27 -0800

    Update normalize.sh

--- normalize.sh
+++ normalize.sh
@@ -1,4 +1,4 @@
-# Makes all tabls two spaces
+# Makes all tabls four spaces
 find . -name '*.java' ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
 # Removes trailing whitespace
 find src -not \( -name .svn -prune -o -name .git -prune \) -type f -print0 | xargs -0 sed -i '' -E "s/[[:space:]]*$//"
